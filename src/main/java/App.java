4f66d1b6c48d815ece6d648e751e37d9ec69f495
Ondicho <ondicho53@gmail.com>
2020-09-11 09:33:44 +0300

    adds a user interface in app.java to pick user input using the command line

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,24 @@
-public class App {
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
+public class App {
+    public static void main(String[] args) {
+        Console myConsole=System.console();
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Input text to be encrypted:");
+
+
+        try {
+            String userInput = bufferedReader.readLine();
+            System.out.println("What encryption key would you like to be used?");
+            int encryptionKey = Integer.parseInt(bufferedReader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
